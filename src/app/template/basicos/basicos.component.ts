73aa32a64941e86a5815a328933e7a6c0d211ad3
Jacob Gutierrez <jacob.gutierrez@newmill.com>
2021-09-26 19:45:02 -0500

    Directiva personalizada

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BasicosComponent implements OnInit {
+  @ViewChild('miFormulario') miFormulario!: NgForm;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  /**
+   * guardar
+   */
+  public guardar() {
+    console.log(this.miFormulario.value);
   }
 
+  /**
+   * nombreEsValido
+  miFormulario.controls.producto?.invalid &&*/
+  public nombreNoEsValido(): boolean {
+    return (
+      this.miFormulario?.controls.producto?.invalid &&
+      this.miFormulario?.controls.producto?.touched
+    );
+  }
+
+  /**
+   * precioNoEsValido
+   */
+  public precioNoEsValido(): boolean {
+    return this.miFormulario?.controls.precio?.touched &&
+     this.miFormulario?.controls.precio?.value < 0;
+  }
 }
--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TemplateRoutingModule } from './template-routing.module';
 import { SwitchesComponent } from './switches/switches.component';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
 import { BasicosComponent } from './basicos/basicos.component';
+import { CustomMinDirective } from './directives/custom-min.directive';
 
 
 @NgModule({
   declarations: [
     SwitchesComponent,
     DinamicosComponent,
-    BasicosComponent
+    BasicosComponent,
+    CustomMinDirective
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     TemplateRoutingModule
