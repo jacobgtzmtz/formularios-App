e15394b5cd6607f57dfc6c1738c1e213db03211c
Jacob Gutierrez <jacob.gutierrez@newmill.com>
2021-09-28 10:49:26 -0500

    formularios reactivos switches terminado

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  public miFormulario: FormGroup = this.$formBuilder.group({
+    genero: [,Validators.required],
+    notificaciones: [false, Validators.required],
+    termycond: [false, Validators.requiredTrue]
+  })
+
+  public persona ={
+    genero: 'F',
+    notificaciones: false
+  }
+
+  constructor(private $formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    //Esta linea funciona solo si todos los campos del objeto coinciden con los del FormGroup
+    //this.miFormulario.setValue(this.persona);
+
+    //Esta linea funciona mejor
+    //this.miFormulario.reset(this.persona);
+
+    //Esta está mejor optimizada:
+    this.miFormulario.reset(
+      {
+        ...this.persona,
+        termycond: false
+      }
+    )
+  }
+
+  /**
+   * guardar
+   */
+  public guardar() {
+    const formvalue = {...this.miFormulario.value};
+    console.log(formvalue);
   }
 
 }
